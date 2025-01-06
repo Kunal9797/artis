e6b3c4cdd955299997e7ae6c2cfc7a6f56e70a85
Kunal <kunal@Mac.lan>
2025-01-06 21:13:19 +0530

    backend fixes 10

---
+++ backend/migrations/20240315000006-create-users.js
@@ -0,0 +1,46 @@
+'use strict';
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true
+      },
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      email: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      role: {
+        type: DataTypes.ENUM('admin', 'user'),
+        defaultValue: 'user'
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+      }
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+}; 
